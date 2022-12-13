7c14a3054ce705f22765375bfd0d7004460753c9
Gabriella Elizondo <gabriellaelizondo@gmail.com>
2022-12-13 18:02:32 -0500

    commit tree f2d3fbb8c9419d8021e371f0e59f015054025851

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+//create and h1 and assign it to the new header variable
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Gabreilla is the champion";
